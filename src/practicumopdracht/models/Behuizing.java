ff1e818ec342815ca0f2edaebb1d7f40a870a9e6
Redouan Bouziza <redouan.bouziza@hva.nl>
2022-10-21 21:59:33 +0200

    Models toegevoegd

---
+++ src/practicumopdracht/models/Behuizing.java
@@ -0,0 +1,78 @@
+/**
+ * @Author: Redouan Bouziza IS205
+ * Master model
+ */
+
+package practicumopdracht.models;
+
+import java.io.Serializable;
+
+public class Behuizing implements Serializable {
+
+    //    Attributen
+    private String soort;
+    private double hoogte;
+    private int serieNummer;
+    private boolean behuizingHeeftTemperedGlass;
+
+    /**
+     * Constructor for Behuizing
+     *
+     * @param soort
+     * @param hoogte
+     * @param serieNummer
+     * @param behuizingHeeftTemperedGlass
+     */
+    public Behuizing(String soort, double hoogte, int serieNummer, boolean behuizingHeeftTemperedGlass) {
+        this.soort = soort;
+        this.hoogte = hoogte;
+        this.serieNummer = serieNummer;
+        this.behuizingHeeftTemperedGlass = behuizingHeeftTemperedGlass;
+    }
+
+    //   Getters and Setters
+    public String getSoort() {
+        return soort;
+    }
+
+    public Double getHoogte() {
+        return hoogte;
+    }
+
+    public int getSerieNummer() {
+        return serieNummer;
+    }
+
+    public boolean heeftBehuizingTemperedGlass() {
+        return behuizingHeeftTemperedGlass;
+    }
+
+    public void setSoort(String soort) {
+        this.soort = soort;
+    }
+
+    public void setHoogte(Double hoogte) {
+        this.hoogte = hoogte;
+    }
+
+    public void setSerieNummer(int serieNummer) {
+        this.serieNummer = serieNummer;
+    }
+
+    public void setBehuizingHeeftTemperedGlass(boolean BehuizingHeeftTemperedGlass) {
+        this.behuizingHeeftTemperedGlass = BehuizingHeeftTemperedGlass;
+    }
+
+    /**
+     * To string methode
+     *
+     * @return toString
+     */
+    public String toString() {
+        return String.format("\nSoort: %s\nHoogte: %3.2f\nSerie nummer: %d\nBehuizing heeft tempered glass: %s",
+                soort,
+                hoogte,
+                serieNummer,
+                behuizingHeeftTemperedGlass ? "Ja" : "Nee");
+    }
+}
---
+++ src/practicumopdracht/models/Component.java
@@ -0,0 +1,70 @@
+/**
+ * @Author: Redouan Bouziza IS205
+ * Detail model
+ */
+
+package practicumopdracht.models;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class Component implements Serializable {
+
+    //    Attributen
+    private transient Behuizing hoortbij;
+    private String naam;
+    private LocalDate datum;
+
+    /**
+     * Constructor for Behuizing
+     * @param hoortbij
+     * @param naam
+     * @param datum
+     */
+    public Component(Behuizing hoortbij, String naam, LocalDate datum) {
+        this.hoortbij = hoortbij;
+        this.naam = naam;
+        this.datum = datum;
+    }
+
+    //   Getters and Setters
+    public Behuizing getHoortbij() {
+        return hoortbij;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public LocalDate getDatum() {
+        return datum;
+    }
+
+    public void setHoortbij(Behuizing hoortbij) {
+        this.hoortbij = hoortbij;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public void setDatum(LocalDate datum) {
+        this.datum = datum;
+    }
+
+    /**
+     * To string methode
+     *
+     * @return toString
+     */
+    public String toString() {
+        return String.format("\nNaam: %s\nDatum: %s",
+                naam,
+                datum);
+    }
+
+
+
+
+
+}
